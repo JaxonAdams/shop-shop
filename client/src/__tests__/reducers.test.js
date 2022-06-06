a1d5fc46457bab9d9a53b867c34d9c54ae1de7ce
Jaxon Adams <jadamsresume1@gmail.com>
2022-06-05 22:11:50 -0600

    set up and tested actions and reducers

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,47 @@
+// import actions
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from '../utils/actions'
+// import reducers
+import { reducer } from '../utils/reducers';
+
+// create a sample of what our global state will look like
+const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1'
+};
+
+describe('Reducers', () => {
+    it('changes state with the UPDATE_PRODUCTS action', () => {
+        let newState = reducer(initialState, {
+            type: UPDATE_PRODUCTS,
+            products: [{}, {}]
+        });
+
+        expect(newState.products.length).toBe(2);
+        expect(initialState.products.length).toBe(0);
+    });
+
+    it('changes state with the UPDATE_CATEGORIES action', () => {
+        let newState = reducer(initialState, {
+            type: UPDATE_CATEGORIES,
+            categories: [{}, {}]
+        });
+
+        expect(newState.categories.length).toBe(2);
+        expect(initialState.categories.length).toBe(1);
+    });
+
+    it('changes state with the UPDATE_CURRENT_CATEGORY action', () => {
+        let newState = reducer(initialState, {
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: '2'
+        });
+
+        expect(newState.currentCategory).toBe('2');
+        expect(initialState.currentCategory).toBe('1');
+    });
+});
